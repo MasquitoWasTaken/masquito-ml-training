a9f763b3b3fd1eba50dcaaf5a3f3f4b1266f5b54
Gabriel Pizarro <gpizarro@javaman.net>
2020-11-30 14:06:56 -0800

    Added timing to test_model_cli.py

--- test_model_cli.py
+++ test_model_cli.py
@@ -1,5 +1,6 @@
 from imageai.Prediction.Custom import CustomImagePrediction
 import os
+import time
 
 model = "model_ex-067_acc-0.953125.h5"
 execution_path = os.getcwd()
@@ -15,8 +16,14 @@ prediction.loadModel(num_objects=3)
 
 while True:
     filename = input("Test file: ")
-    # Get and display results
+
+    # Get and display results, time the process
+    t_start = int(round(time.time() * 1000))
     predictions, probabilities = prediction.predictImage(
         os.path.join(execution_path, "test_images/" + filename), result_count=5)
+    t_stop = int(round(time.time() * 1000))
+    t_delta = t_stop - t_start
+
+    print("Running time:", str(t_delta) + "ms")
     for eachPrediction, eachProbability in zip(predictions, probabilities):
-        print(eachPrediction, " : ", eachProbability)
+        print(eachPrediction + ":", eachProbability)
